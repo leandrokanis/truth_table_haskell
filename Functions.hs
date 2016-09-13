0b55bc3d1e76df21c38b79837f4c24aeaa8b77cb
Leandro Alves <leandrosustenido@gmail.com>
2016-09-13 18:08:44 -0300

    basic boolean function

---
+++ Functions.hs
@@ -0,0 +1,8 @@
+module Functions where
+
+plus :: Int -> Int -> Int
+plus = (+)
+
+a = False
+b = True
+b = False
---
+++ table.hs
@@ -0,0 +1,8 @@
+import Functions
+
+main = do
+    let res = plus 1 2
+    putStrLn $ "1+2 = " ++ show res
+
+    let res1 = a || b
+    putStrLn $ show res1
