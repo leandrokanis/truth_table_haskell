8fff0ea82306a4e068f0ecc74d086a8d7edb68c6
Leandro Alves <leandrosustenido@gmail.com>
2016-09-15 21:09:04 -0300

    creating print table

--- main.hs
+++ main.hs
@@ -5,7 +5,7 @@ import Unduplicate
 import Combinations
 import Truthtable
 import Operators
-
+import PrintTable
 
 main = do
     printtbl $ truthtable $ read "not ((A or B) and (A or C) and (B or C))"
