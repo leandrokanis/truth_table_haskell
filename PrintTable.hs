42e0d557a5c27e18e380d05cb070365d03d45294
Leandro Alves <leandrosustenido@gmail.com>
2016-09-16 00:30:11 -0300

    table editing

--- PrintTable.hs
+++ PrintTable.hs
@@ -2,15 +2,15 @@ module PrintTable where
 import Functions
 
 showcell :: (Variable, Bool) -> String
-showcell (v,b) = v : "=" ++ show b
+showcell (v,b) = v : ". " ++ show b
 
 showrow :: [(Variable, Bool)] -> Bool -> String
 showrow []     b = show b
-showrow [a]    b = showcell a ++ " => " ++ show b
-showrow (a:as) b = showcell a ++ " && " ++ showrow as b
+showrow [a]    b = showcell a ++ " = " ++ show b
+showrow (a:as) b = showcell a ++ " | " ++ showrow as b
 
 printrow :: ([(Variable, Bool)], Bool) -> IO ()
 printrow = putStrLn . uncurry showrow
 
 printtbl :: [([(Variable, Bool)], Bool)] -> IO ()
-printtbl = mapM_ printrow
+printtbl = mapM_ printrow 
